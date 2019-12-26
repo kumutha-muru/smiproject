b32822b05edb766b96b565171e362adccc430cab
sangeetha <sangeetha.aroohi@gmail.com>
2019-12-26 17:16:27 +0530

    final updated

--- src/app/traineeservice.service.ts
+++ src/app/traineeservice.service.ts
@@ -12,7 +12,7 @@ export class TraineeserviceService {
   private Url: string;
   constructor(private http: HttpClient) {
 
-    this.Url = 'http://192.168.6.25:8080/smi';
+    this.Url = 'https://smi-training-2020.herokuapp.com/smi';
   }
   public getCourseDetails(): Observable<Issue[]> {
     return this.http.get<Issue[]>(this.Url + "/get");
